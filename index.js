e6ebc6a5adbb00090ed75ed6feb9ee716e3b2a04
ahmed-al-farouq <mediocre23534@gmail.com>
2023-04-24 08:50:36 +0200

    Finish navbar design, and functions

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-let menu = document.getElementById("menu");
-let menuBtn = document.getElementById("menu-btn");
+// Navbar
+const menu = document.getElementById("menu");
+const menuBtn = document.getElementById("menu__toggler");
+const subMenuToggler = document.querySelectorAll(".sub__menu__toggler");
 
 menuBtn.addEventListener("click", () => {
   menu.classList.toggle("close");
+});
+
+subMenuToggler.forEach((parent) => {
+  parent.addEventListener(
+    "click",
+    () => {
+      if (window.innerWidth >= 991) return;
+      const subMenu = parent.querySelector(".sub__menu");
+      const openIcon = parent.querySelector(".fa-plus");
+      const closeIcon = parent.querySelector(".fa-minus");
+      openIcon.classList.toggle("hidden");
+      closeIcon.classList.toggle("hidden");
+      subMenu.classList.toggle("close");
+    },
+    true
+  );
 });
