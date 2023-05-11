8e9ee9b243cf71fa445731f37ebb29b86148071c
ahmed-al-farouq <mediocre23534@gmail.com>
2023-05-11 04:11:33 +0200

    Add Favicons

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -41,7 +42,17 @@ module.exports = {
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./src/assets/img/favicon",
+          to: "./assets/img/favicon",
+        },
+      ],
+    }),
+  ],
   optimization: {
     splitChunks: {
       chunks: "all",
