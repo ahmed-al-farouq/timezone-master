45d2cef55e26d5cf8ad3b34dc6141931501d131b
ahmed-al-farouq <mediocre23534@gmail.com>
2023-05-13 01:50:52 +0200

    update web config

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,6 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
-
       {
         test: /\.(?:js|mjs|cjs)$/,
         exclude: /node_modules/,
