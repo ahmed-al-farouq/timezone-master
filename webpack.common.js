0c8c5e1995b4e8ff8ddfccedec553a7777c08a09
ahmed-al-farouq <mediocre23534@gmail.com>
2023-05-27 04:24:28 +0200

    comment babel loader

--- webpack.common.js
+++ webpack.common.js
@@ -18,19 +18,19 @@ module.exports = {
                 type: "asset/resource",
             },
 
-            {
-                test: /\.(?:js|mjs|cjs)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [["@babel/preset-env", { targets: "defaults" }]],
-                    },
-                },
-                generator: {
-                    filename: "assets/js/[name].bundle.[contenthash].js",
-                },
-            },
+            // {
+            //     test: /\.(?:js|mjs|cjs)$/,
+            //     exclude: /node_modules/,
+            //     use: {
+            //         loader: "babel-loader",
+            //         options: {
+            //             presets: [["@babel/preset-env", { targets: "defaults" }]],
+            //         },
+            //     },
+            //     generator: {
+            //         filename: "assets/js/[name].bundle.[contenthash].js",
+            //     },
+            // },
         ],
     },
     plugins: [
