4be84d216dd87c1a16a4ef50cfcaddd35146c050
ahmed-al-farouq <mediocre23534@gmail.com>
2023-05-30 08:48:58 +0200

    compress images

--- webpack.common.js
+++ webpack.common.js
@@ -43,6 +43,9 @@ module.exports = {
               gifsicle: {
                 interlaced: false,
               },
+              webp: {
+                quality: 75
+              }
             },
           },
         ],
